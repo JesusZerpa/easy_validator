8be286b7c1854dc744839427f8dded387cd1a7a5
jesbram <jesus26abraham1996@gmail.com>
2022-10-04 12:19:49 -0400

    update

--- easy_validator/__init__.py
+++ easy_validator/__init__.py
@@ -1,106 +1,244 @@
 
 class Validator:
-	def __init__(self,framework=None,**validators):
-		self.__validators=validators
-		self.__framework=framework
-		self._required=[]
-		
-		for elem in validators:
-			
+    def __init__(self,framework=None,**validators):
+        self.__validators=validators
+        self.__framework=framework
+        self._required=[]
+        
+        for elem in validators:
+            
 
-			def validator(required):
-				if required:
-					self._required.append(required)
+            def validator(required):
+                if required:
+                    self._required.append(required)
 
-				return validators[elem](
-					required=required,
-					**config)
-			return validator
-
-
+                return validators[elem](
+                    required=required,
+                    **config)
+            return validator
 
 
-			setattr(self,elem,
-				lambda required=False,**config:validator(required,**config))
 
 
-	def __call__(self,validator):
-		import inspect
-		from functools import wraps
-		def decorator(fn):
-			
-			if self.__framework=="flask":
-				from flask import jsonify
-				@wraps(fn)
-				def wrapper(*params,**kwargs):
-	
-					try:
-						validator()
-					except ValidationError as e:
+            setattr(self,elem,
+                lambda required=False,**config:validator(required,**config))
 
-						return jsonify({"ValidationError":str(e)}),500
 
-					return {"ValidationError":str(e)}
-				return fn(*params,**kwargs)
+    def __call__(self,validator):
+        import inspect
+        from functools import wraps
+        def decorator(fn):
+            
+            if self.__framework=="flask":
+                from flask import jsonify
+                @wraps(fn)
+                def wrapper(*params,**kwargs):
+    
+                    try:
+                        validator()
+                    except ValidationError as e:
 
-		
-			elif self.__framework=="quart":
-				from quart import jsonify
-				@wraps(fn)
-				async def wrapper(*params,**kwargs):
+                        return jsonify({"ValidationError":str(e)}),500
+                    except ValidationRequired as e:
+                        return jsonify({"ValidationRequired":str(e)}),500
 
-					try:
-						await validator()
-						
-					except ValidationError as e:
-						
-						return jsonify({"ValidationError":str(e)}),500
-					return await fn(*params,**kwargs)
-				
-		
 
-			return wrapper
-		return decorator
+                    return {"ValidationError":str(e)}
+                return fn(*params,**kwargs)
 
-	def validate(self,data,schema):
-		for elem in schema:
-			path=elem.split(".")
+        
+            elif self.__framework=="quart":
+                from quart import jsonify
+                @wraps(fn)
+                async def wrapper(*params,**kwargs):
 
-			item=data[path[0]]
-			for p in path[1:]:
-				item=item[p]
-		
-			if not schema[elem](item):
-				for valid in dir(self):
-					if not valid.startswith("_") and valid not in ["validate"]:
-						if valid==schema[elem].__name__:
-							raise ValidationError(f"Dato '{elem}' no es valido para {valid}")
-		self._required=[]
-		return True
+                    try:
+                        await validator()
+                        
+                    except ValidationError as e:
+                        print({"ValidationError":str(e)})
+                        return jsonify({"ValidationError":str(e)}),500
+                    except ValidationRequired as e:
+                        return jsonify({"ValidationRequired":str(e)}),500
+
+                    return await fn(*params,**kwargs)
+                
+        
+
+            return wrapper
+        return decorator
+
+    def validate(self,data,schema):
+        for index,elem in enumerate(schema):
+            path=elem.split(".")
+            required=False
+            
+            if "required" in dir(schema[elem]):
+                required=True
+            
+            if path[0] not in data and required:
+                raise ValidationRequired(f"Validación requerida para '{path[0]}'")
+            if path[0] not in data:
+                continue
+            item=data[path[0]]
+            _path=path[0]
+            for p in path[1:]:
+                _path+="."+p
+                if type(item)==dict:
+                    if p not in item.keys():
+                        
+                        raise ValidationError(f"key '{_path}' not in {item.keys()}")
+                    item=item[p]
+                elif p.isdigit():
+                    item=item[int(p)]
+               
+
+            if callable(schema[elem]) and not schema[elem](item):
+                
+                for valid in dir(self):
+                 
+                    if not valid.startswith("_") and valid not in ["validate"]:
+                        if valid==schema[elem].__name__:
+                            raise ValidationError(f"Dato '{elem}' no es valido para {valid}")
+            elif type(schema[elem]) in [list,tuple]:
+                l=[]
+                for v in schema[elem]:
+                    l.append(v(item))
+              
+                if not any(l):
+                    raise ValidationError(f"Dato '{elem}' no es valido")
+
+
+        self._required=[]
+        return True
 
 class UtilValidator(Validator):
-	
-	def is_isoformat(self,required=False):
-		self._required.append(required)
 
-		def is_isoformat(date_string):
-			"""Valida si una cadena de texto es isoformat 
+    def is_structure(self,structure:dict,required=False):
+     
 
-			>>> is_isoformat('2022-09-30T00:25:40.338953')
-			True
-			"""
-			from datetime import datetime
-			try:
-			    datetime.fromisoformat(date_string)
-			    return True
-			except ValueError:
-				return False
+        def is_structure(data:dict):
 
-		return is_isoformat
+            def recursive(data,structure):
+                l=[]
+                for elem in data:
+                    if callable(structure[elem]):
+                        l.append(structure[elem](data[elem]))
+                    elif type(data[elem])==dict:
+                        l.append(recursive(data[elem],structure[elem]))
+                return all(l)
+
+            l=[]
+            for elem in data:
+
+                if callable(structure[elem]):
+                    l.append(structure[elem](data[elem]))
+                elif type(data[elem])==dict:
+                    l.append(recursive(data[elem],structure[elem]))
+            return all(l)
+        is_structure.required=required
+        return is_structure
+    
+    def is_image_file(self,required=False):
+        def is_image_file(string):
+            for elem in [".jpg",".png",".bmp",".svg",".jpeg"]:
+
+                if string.lower().endswith(elem):
+                    return True
+            return False
+        is_image_file.required=required
+        return is_image_file
+
+    def is_extension(self,extend:list,required=False):
+        def is_extension(string):
+            for elem in extend:
+                if string.lower().endswith(elem):
+                    return True
+            return False
+        is_image_file.required=required
+        return is_image_file
+
+    def exists_register(self,session,model,required=False,**fields):
+        def exists_register(string):
+            for elem in extend:
+                query={}
+                for field in fields:
+                    query[getattr(model,field)]=fields[field]
+                instance=session.query(model).filter(query).first()
+                if instance:
+                    return True
+            return False
+        exists_register.required=required
+        return exists_register
+
+    def is_isoformat(self,required=False):
+        
+
+        def is_isoformat(date_string):
+            """Valida si una cadena de texto es isoformat 
+
+            >>> is_isoformat('2022-09-30T00:25:40.338953')
+            True
+            """
+            from datetime import datetime
+            try:
+                datetime.fromisoformat(date_string)
+                return True
+            except ValueError:
+                return False
+        is_isoformat.required=required
+        return is_isoformat
+
+    def is_zisoformat(self,required=False):
+        
+
+        def is_zisoformat(date_string):
+            """Valida si una cadena de texto es isoformat 
+
+            >>> is_isoformat('2022-09-30T00:25:40.338953')
+            True
+            """
+            from datetime import datetime
+            try:
+                datetime.fromisoformat(date_string.replace("Z","+00:00"))
+                return True
+            except ValueError:
+                return False
+        is_zisoformat.required=required
+        return is_zisoformat
+
+def replace(data,path,fn):
+    path=path.split(".")
+    item=data[path[0]]
+    for k,p in enumerate(path[1:]):
+
+        if p in item:
+            
+            if k+1==len(path[1:]):
+                item[p]=fn(item[p])
+
+            else:
+                item=item[p]
+        else:
+            break
+
+def replace_default(data:dict,default:dict):
+    def recusive(data,default):
+        for elem in default:
+            if elem not in data:
+                data[elem]=default[elem]
+            elif type(data[elem])==dict:
+                recusive(data[elem],default[elem])
+    recusive(data,default)
 
 class ValidationError(Exception):
-	pass
+    pass
+class ValidationRequired(Exception):
+    pass
+def check(fn,required=False):
+    fn.required=required
+    return fn
 util_validator=UtilValidator()
 if __name__=="__main__":
-	import doctest
-	doctest.testmod(verbose=True)
+    import doctest
+    doctest.testmod(verbose=True)
--- easy_validator/tests/test_1.py
+++ easy_validator/tests/test_1.py
@@ -8,7 +8,9 @@ def app():
     app.config.update({
         "TESTING": True,
     })
+
     util_validator=UtilValidator(framework="quart")
+    
     @util_validator
     async def my_validator():
         util_validator.validate(await request.json,{
@@ -31,9 +33,6 @@ def client(app):
 @pytest.fixture()
 def client(app):
     return app.test_client()
-
-
-
 
 
 def test_isoformat():
@@ -59,12 +58,59 @@ def test_not_isoformat():
         assert True
 
 
+def test_is_structure():
+    from easy_validator import util_validator,ValidationError
+    dias=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+    try:
+        valid=util_validator.validate({
+            "horarios":{dia:{
+                "start":"",
+                "end":""
+                } 
+            for dia in dias}
+        },
+        {
+           "horarios":util_validator.is_structure({
+            dia:{
+                "start":lambda x: type(x)==str,
+                "end":lambda x:type(x)==str
+                } 
+            for dia in dias}),
+        })
+    except ValidationError as e:
+        assert False
+    assert valid
+    
+def test_not_is_structure():
+    from easy_validator import util_validator,ValidationError
+    dias=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+    try:
+        util_validator.validate({
+            "horarios":{dia:{
+                "start":20,
+                "end":False
+                } 
+            for dia in dias}
+        },
+        {
+           "horarios":util_validator.is_structure({
+            dia:{
+                "start":lambda x: type(x)==str,
+                "end":lambda x:type(x)==str
+                } 
+            for dia in dias}),
+        })
+    except ValidationError as e:
+        assert True
+
 
 
 
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
+
+
 @pytest.mark.asyncio
 async def test_request_example(client):
     response = await client.post("/post",json={
