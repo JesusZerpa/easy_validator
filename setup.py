fb23be9712abb77b9bd4479452bb1fd5bba70e26
jesbram <jesus26abraham1996@gmail.com>
2023-01-04 12:28:53 -0400

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-	name="easy_validator",
-	packages=["easy_validator"],
+	name="my_validator",
+	packages=["my_validator"],
 	version="0.0.1",
 	description="Libreria de validación de datos",
 	author="Jesus Zerpa",
 	author_email="jesus26abraham1996@gmail.com",
-	url="https://zerpatechnology.com",
-	keywords=["easy_validator"],
+	url="https://zerpatechnology.com/projects/easy_validator",
+	keywords=["easy validator","flask","quart"],
 	scripts=[],
 	entry_points={
 
